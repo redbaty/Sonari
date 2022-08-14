f9990ace7b4579d81765af46a11798d02fa311f9
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2022-08-14 01:50:06 -0300

    Supports crunchyroll authentication on CrunchyrollApiServiceFactory

--- Sonari.Crunchyroll/CrunchyrollApiServiceFactory.cs
+++ Sonari.Crunchyroll/CrunchyrollApiServiceFactory.cs
@@ -20,17 +20,33 @@ public class CrunchyrollApiServiceFactory
     private ILogger<CrunchyrollApiServiceFactory> Logger { get; }
 
     private const string ChaveCache = "crunchyroll_service";
+    
+    public string? Token { get; set; }
 
     public CrunchyrollApiService GetService() => Cache.Get<CrunchyrollApiService>(ChaveCache) ??
                                                  throw new InvalidOperationException("Service has not been created");
 
-    public async Task CreateUnauthenticatedService()
+    public async Task CreateAuthenticatedService(string username, string password)
     {
-        var token = await CrunchyrollApiAuthenticationService.GetAccessToken();
+        Token = await CrunchyrollApiAuthenticationService.GetAccessToken(username, password);
+        
         var httpClient = new HttpClient
         {
             BaseAddress = new Uri("https://beta-api.crunchyroll.com/"),
-            DefaultRequestHeaders = { { "Authorization", $"Bearer {token}" } }
+            DefaultRequestHeaders = { { "Authorization", $"Bearer {Token}" } }
+        };
+
+        Logger.LogInformation("Created authenticated API service");
+        Cache.Set(ChaveCache, new CrunchyrollApiService(httpClient));
+    }
+    
+    public async Task CreateUnauthenticatedService()
+    {
+        Token = await CrunchyrollApiAuthenticationService.GetAccessToken();
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://beta-api.crunchyroll.com/"),
+            DefaultRequestHeaders = { { "Authorization", $"Bearer {Token}" } }
         };
 
         Logger.LogInformation("Created unauthenticated API service");
